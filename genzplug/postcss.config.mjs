485f85d6ad99f560e8817d9644910d5c8dc08b2e
sanjay karki <chettripasaa@gmail.com>
2025-08-17 10:48:00 -0700

    Initial commit

---
+++ genzplug/postcss.config.mjs
@@ -0,0 +1,18 @@
+const config = {
+  plugins: ["@tailwindcss/postcss"],
+};
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+export default config;
